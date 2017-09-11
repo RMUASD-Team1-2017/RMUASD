dcd5dc0d0e7ba28a68cf708ed88a5442d5633bfc
loleb <loleb16@student.sdu.dk>
2017-09-11 10:41:08 +0200

    Modified risk assessment

--- software/gcs_ws/src/aed_gcs_logic/src/risk_assessment.py
+++ software/gcs_ws/src/aed_gcs_logic/src/risk_assessment.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 import rospy
 
-from std_msgs.msg import String
+from std_msgs.msg import String 
+from std_msgs.msg import Int8
+from std_msgs.msg import Float32
 
 class risk_analyzer:
     """Risk Analyzer class"""
 
     def __init__(self):
-        self.risk_metric_pub = rospy.Publisher('/risk_assessment/risk_metric', String, queue_size=1)
+        #self.risk_metric_pub = rospy.Publisher('/risk_assessment/risk_metric', Int8, queue_size=1)		# using int metric
+        self.risk_metric_pub = rospy.Publisher('/risk_assessment/risk_metric', Float32, queue_size=1)		# using float metric
 
     def setup_analyzer(self):
         print "Settting up analyzer"
@@ -15,10 +18,16 @@ class risk_analyzer:
     def analyze(self):
         print "Analyzing..."
 
-        # Do analyze here
-        # ...
+        # Risk analysis goes in here and should depend on several factors:
+        # 	- Obstacles, weather, etc.
+        #	- Should be something like 'weather condition + other factors - ...'
+        #	- risk_metric should have some upper bound, such as 100
+        #	- Need to get weather conditions and obstacle conditions from somewhere
+        weather_conditions = 5
+        obstacle_conditions = 5
 
-        risk_metric = "100 percent clear"
+        # compute final risk metric:
+        risk_metric = weather_conditions + obstacle_conditions
         self.risk_metric_pub.publish(risk_metric)
 
 if __name__ == "__main__":
