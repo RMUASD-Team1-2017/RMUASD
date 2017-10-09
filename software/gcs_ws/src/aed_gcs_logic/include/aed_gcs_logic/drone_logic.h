837d524d5d44be0fb46109bd83f138c9dad9ad92
Nikolaj <n.bach@sol.dk>
2017-10-09 00:27:58 -0700

    update arming and check for mission

--- software/gcs_ws/src/aed_gcs_logic/include/aed_gcs_logic/drone_logic.h
+++ software/gcs_ws/src/aed_gcs_logic/include/aed_gcs_logic/drone_logic.h
@@ -4,6 +4,7 @@
 #include "ros/ros.h"
 #include "mavros_msgs/Waypoint.h"
 #include "mavros_msgs/WaypointPush.h"
+#include "mavros_msgs/WaypointPull.h"
 #include "mavros_msgs/CommandCode.h"
 #include "mavros_msgs/CommandBool.h"
 #include "mavros_msgs/SetMode.h"
@@ -12,6 +13,8 @@
 #include "mavros_msgs/ParamGet.h"
 #include "geometry_msgs/TwistStamped.h"
 #include "geometry_msgs/PoseStamped.h"
+#include "sensor_msgs/NavSatFix.h"
+#include <std_msgs/Float64.h>
 #include <GeographicLib/Geodesic.hpp>
 
 #include "aed_gcs_logic/waypoints.h"
@@ -32,15 +35,18 @@ using namespace GeographicLib;
 // #define MISSION_READY       4
 // #define ON_MISSION          5
 // #define MISSION_DONE        6
+#define max_dist_between_waypoints  500
 
 enum droneState {
     IDLE = 1,
     FAILED = 2,
 	SEND_MISSION = 3,
     ARM = 4,
-    START_MISSION = 5,
-    ON_MISSION = 6,
-    MISSION_DONE = 7
+	WAITING_STATE = 5,
+    AUTO_MISSION = 6,
+	START_MISSION = 7,
+    ON_MISSION = 8,
+    MISSION_DONE = 9
 };
 
 struct path{
@@ -66,9 +72,11 @@ class drone_handler
         ros::Subscriber state_sub; // n.subscribe<mavros_msgs::State>("mavros/state", 1, state_cb);
         ros::Subscriber mission_sub; // n.subscribe<mavros_msgs::State>("mavros/state", 1, state_cb);
         ros::Publisher velocity_pub; // n.subscribe<mavros_msgs::State>("mavros/state", 1, state_cb);
-
+		ros::ServiceClient pull_client; // = n.serviceClient<mavros::WaypointPull>("/mission/WaypointPull");
+		ros::Subscriber nav_sat_fix_gps; // = n.subscribe<sensor_msgs::NavSatFix>("mavros/global_position/global", 1, &drone_handler::mission_callback, this);
         // Callbacks
         void current_state_callback(const mavros_msgs::State::ConstPtr& data);
+		void gps_callback(const sensor_msgs::NavSatFix::ConstPtr& data);
         void mission_callback(const aed_gcs_logic::waypoints::ConstPtr& data);
 		double calc_dist_between_waypoints(double lat1, double lat2, double long1, double long2);
 
@@ -77,12 +85,17 @@ class drone_handler
         bool off_board_requested;
         bool connected;
         bool armed;
+		double latitude;
+		double longitude;
+		double altitude;
         std::string mode;
-		double iterator = 0;
-		int iterator_ceil = 0;
-		double dist_between_waypoints_m;
-		int max_dist_between_waypoints = 500;
+		double current_height = 0;
+		double start_height = 0;
 		
+		
+		
+		ros::Time start_time;
+		ros::Time arm_time;
 
 
 		
--- software/gcs_ws/src/aed_gcs_logic/src/drone_logic.cpp
+++ software/gcs_ws/src/aed_gcs_logic/src/drone_logic.cpp
@@ -8,6 +8,10 @@ drone_handler::drone_handler()
     this->connected = false;
     this->armed = false;
     this->mode = "None";
+	this->latitude = 0;	
+	this->longitude = 0;
+	this->altitude = 0;
+
 
     this->param_set_client = n.serviceClient<mavros_msgs::ParamSet>("mavros/param/set");
     this->mission_push_client = n.serviceClient<mavros_msgs::WaypointPush>("mavros/mission/push");
@@ -16,6 +20,8 @@ drone_handler::drone_handler()
     this->state_sub = n.subscribe<mavros_msgs::State>("mavros/state", 1, &drone_handler::current_state_callback, this);
     this->mission_sub = n.subscribe<aed_gcs_logic::waypoints>("path", 1, &drone_handler::mission_callback, this);
     this->velocity_pub = n.advertise<geometry_msgs::PoseStamped>("mavros/setpoint_position/local", 1);
+	this->pull_client = n.serviceClient<mavros_msgs::WaypointPull>("mavros/mission/pull");
+	this->nav_sat_fix_gps = n.subscribe<sensor_msgs::NavSatFix>("mavros/global_position/global", 1, &drone_handler::gps_callback, this);
 }
 
 drone_handler::~drone_handler(){
@@ -53,7 +59,7 @@ bool drone_handler::setup()
 double drone_handler::calc_dist_between_waypoints(double lat1, double lat2, double long1, double long2)
 
 {
-
+	double dist_between_waypoints_m;
 
 	const Geodesic& geod = Geodesic::WGS84();
 
@@ -70,7 +76,16 @@ void drone_handler::current_state_callback(const mavros_msgs::State::ConstPtr& d
 {
     this->connected = data->connected;
     this->armed = data->armed;
-    this->mode = data->mode;  // indicates that is a variable from this class
+    this->mode = data->mode;  // indicates that it is a variable from this class
+}
+
+void drone_handler::gps_callback(const sensor_msgs::NavSatFix::ConstPtr& data)
+{
+    this->latitude = data->latitude;
+	this->longitude = data->longitude;
+	this->altitude = data->altitude;
+	
+	// indicates that it is a variable from this class
 }
 
 void drone_handler::mission_callback(const aed_gcs_logic::waypoints::ConstPtr& data)
@@ -95,27 +110,29 @@ void drone_handler::mission_callback(const aed_gcs_logic::waypoints::ConstPtr& d
             temp_wp.is_current = false;
 
             for(int i = 1; i < data->path.size() - 1; i++){
-
+				double iterator = 0;
+				int iterator_ceil = 0;
 
 				if(calc_dist_between_waypoints(data->path[i-1].latitude,data->path[i].latitude,data->path[i-1].longitude,data->path[i].longitude)>max_dist_between_waypoints){
 					iterator = (calc_dist_between_waypoints(data->path[i-1].latitude,data->path[i].latitude,data->path[i-1].longitude,data->path[i].longitude)/max_dist_between_waypoints);
 					iterator_ceil = ceil( iterator );
 					std::cout<<" number "<<iterator_ceil<<std::endl;
 					for(int counter = 1 ; counter<iterator_ceil; counter++){
-
+					
 						temp_wp.x_lat =  data->path[i-1].latitude + (((data->path[i].latitude - data->path[i-1].latitude)/iterator_ceil)*counter);
 						std::cout<<" latx "<<temp_wp.x_lat<<std::endl;
                		 	temp_wp.y_long = data->path[i-1].longitude + (((data->path[i].longitude - data->path[i-1].longitude)/iterator_ceil)*counter);
 						std::cout<<" longy "<<temp_wp.y_long<<std::endl;
                 		temp_wp.z_alt = 50;
 						mission_srv_temp.request.waypoints.push_back(temp_wp);
+					//	std::cout<<" waypoints latitude "<<temp_wp.x_lat<< " longitude "<<temp_wp.y_long<<std::endl;
 					}
 				}
-
+				
                 temp_wp.x_lat = data->path[i].latitude;
                 temp_wp.y_long = data->path[i].longitude;
                 temp_wp.z_alt = 50;
-
+				//std::cout<<" waypoints latitude "<<temp_wp.x_lat<< " longitude "<<temp_wp.y_long<<std::endl;
                 mission_srv_temp.request.waypoints.push_back(temp_wp);
                 std::cout << "Point " << i << ": " << temp_wp.x_lat << "," << temp_wp.y_long << std::endl;
             }
@@ -158,7 +175,10 @@ void drone_handler::mission_callback(const aed_gcs_logic::waypoints::ConstPtr& d
 
 void drone_handler::run_state_machine()
 {
+
+	
 //calc_dist_between_waypoints(5,6,6,5);
+
     switch(this->state){
         case IDLE:{
             /* IDLE STATE */
@@ -206,30 +226,92 @@ void drone_handler::run_state_machine()
                 this->arming_client.call(arm_msg);
                 if(arm_msg.response.success){
                     std::cout << "Armed sent" << std::endl;
+					this->arm_time = ros::Time::now();
+					this->state = WAITING_STATE;		
                 }
             }
             else{
-                this->state = START_MISSION;
+                this->state = WAITING_STATE;
             }
             break;}
-        case START_MISSION:{
-            /* START MISSION */
+		
+		case WAITING_STATE:{
+            /* WATING STATE */
+			// wait until the arm is registered
+			if(this->armed){   // if armed go to next state.
+				std::cout << "Armed registered" << std::endl; 
+                this->state = AUTO_MISSION;
+            }            
+			else if(ros::Time::now() - arm_time > ros::Duration(5.0)){
+                //mavros_msgs::CommandBool arm_msg;
+                //arm_msg.request.value = true;
+                //this->arming_client.call(arm_msg);
+				this->state = ARM;
+               
+            }
+            
+            break;}
+		
+		
+        case AUTO_MISSION:{
+            /* AUTO MISSION */
             mavros_msgs::SetMode mission_msg;
             mission_msg.request.custom_mode = "AUTO.MISSION";
             this->set_mode_client.call(mission_msg);
             if(mission_msg.response.mode_sent){
-				std::cout << "Mission Started!" << std::endl;
-                this->state = ON_MISSION;
+				this->start_height = altitude;
+				this->start_time = ros::Time::now();
+				std::cout<<" start height "<<start_height<<std::endl;
+				std::cout << "Mission Set to Auto!" << std::endl;
+                this->state = START_MISSION;
             }
             else{
                 this->state = FAILED;
             }
             break;}
+
+		case START_MISSION:{
+            /* START MISSION */
+            
+            this->current_height = altitude;
+            if(start_height + 5 < current_height){  // when the drone is 5 meters over its start position, it is assumed, that the mission is started succesfully. 
+				
+				std::cout << "Mission Started!" << std::endl;
+                this->state = ON_MISSION; // the drone is in the air
+            }
+			else if(ros::Time::now() - start_time > ros::Duration(10.0)){
+				this->state = FAILED; // if not the drone is in the air after 10 seconds, it failed. 
+			}
+           // else{
+             //   this->state = FAILED;
+           // }
+            break;}
+
         case ON_MISSION:{
-            if(!this->armed){
+			// be sure that the drone has been in air
+			//std::cout<<" mode is: "<<mode<<std::endl;
+			//std::cout<<" latitude  "<<latitude<<" longitude "<<longitude<<" altitude "<<altitude<<std::endl;
+			
+			
+			if(mode == "AUTO.MISSION"){
+
+			mavros_msgs::WaypointPull srv_pull;
+
+  				if (pull_client.call(srv_pull))
+ 				 {
+  				 // ROS_INFO("Received Waypoints: %iu", (uint32_t)srv_pull.response.wp_received);
+ 				 }
+ 				 else
+  				{
+    			ROS_ERROR("Failed to call service add_two_ints");
+   				 //return -1;
+  				}
+
+           		if(!this->armed){
                 std::cout << "Drone has finished mission" << std::endl;
                 this->state = MISSION_DONE;
-            }
+            	}
+			}
             break;}
         case MISSION_DONE:
             /* DO NOTHING */
