f37bbc1e84e160bd676413bd7cf4ae87721ad5df
StefanRvO <stefan@stefanrvo.dk>
2017-10-31 07:58:57 +0100

    Locate directory of script in start_uasd_sim and normal_operation_test instead of just using pwd. Newest version of WebUI where unittests pass. Added full system unit test to travis.yml.

--- tests/normal_operation_test.sh
+++ tests/normal_operation_test.sh
@@ -3,6 +3,7 @@ networkgateway="10.40.0.1"
 simulation_ip="10.40.0.2"
 webui_ip="10.40.0.3"
 set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker network create --gateway ${networkgateway} --subnet ${networkgateway}/24 ${networkname} || true
 docker run -d --name webui --network ${networkname} --ip ${webui_ip} -e rmq_host=${webui_ip} stefanrvo/rmuasdweb:latest
 docker run -d --name simulation --network ${networkname} --ip ${simulation_ip} stefanrvo/rmuasd_simulation:latest
@@ -11,8 +12,8 @@ docker run -d --name drone --network ${networkname} -e webui_ip=${webui_ip} -e s
 docker run -d --name gcs --network ${networkname} -e mavroshost=${simulation_ip}  -e rmq_host=${webui_ip} stefanrvo/rmuasd_groundcontrol:latest
 echo "Containers started. Waiting 20 seconds for system to stabilize."
 sleep 20
-mission_id=$(python Tools/start_mission.py --lat 55.556966 --lon 10.110615 --requesturl http://$webui_ip:8000/EmergencyUser/DroneDispatch/ --accepturl http://$webui_ip:8000/EmergencyControl/control/)
+mission_id=$(python $DIR/Tools/start_mission.py --lat 55.556966 --lon 10.110615 --requesturl http://$webui_ip:8000/EmergencyUser/DroneDispatch/ --accepturl http://$webui_ip:8000/EmergencyControl/control/)
 echo "Mission ID is $mission_id"
-python Tools/MissionMonitor.py --id $mission_id --locationurl http://$webui_ip:8000/EmergencyControl/mission_queue_json/True/ --goal_precision 10 --goal_height 3 --max_mission_time 600
+python $DIR/Tools/MissionMonitor.py --id $mission_id --locationurl http://$webui_ip:8000/EmergencyControl/mission_queue_json/True/ --goal_precision 10 --goal_height 3 --max_mission_time 600
 docker stop simulation webui drone gcs
 docker rm simulation webui drone gcs
