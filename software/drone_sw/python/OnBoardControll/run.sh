034d8c0a6c6661a38db481d2f34c4d56a7791562
StefanRvO <stefan@stefanrvo.dk>
2017-11-09 18:39:18 +0100

    Added serbus to Onboard software requirements.

--- software/drone_sw/python/OnBoardControll/run.sh
+++ software/drone_sw/python/OnBoardControll/run.sh
@@ -6,5 +6,4 @@ socat -d -d  pty,link=/dev/randin,rawer,b57600 pty,link=/dev/randout,rawer,b5760
 socat -d -d  udp:${simulation_ip}:14554,bind=:14541 pty,link=/dev/PX4,rawer,b921600 &
 sleep 2
 cat /dev/urandom >> /dev/randin &
-python main.py --rmquser drone --rmqpass drone --rmqhost ${webui_ip} --loglevel DEBUG --ignoregps --gcsport /dev/randout 
---mavport /dev/PX4 --syslog 0 --ledmode DUMMY
+python main.py --rmquser drone --rmqpass drone --rmqhost ${webui_ip} --loglevel DEBUG --ignoregps --gcsport /dev/randout --mavport /dev/PX4 --syslog 0 --ledmode DUMMY
