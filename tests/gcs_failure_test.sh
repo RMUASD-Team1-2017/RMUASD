d8779befe0dd1c251a74d338b403b462284851f2
StefanRvO <stefan@stefanrvo.dk>
2017-11-11 23:31:26 +0100

    Attempt to fix build not failing on mission_monitor returning non-zero

--- tests/gcs_failure_test.sh
+++ tests/gcs_failure_test.sh
@@ -3,7 +3,8 @@ echo "Running GCS failure test"
 $DIR/start_containers.sh slow_bat_drain
 mission_id=$(python $DIR/Tools/start_mission.py --lat 55.562747 --lon 10.113384 --requesturl http://$webui_ip:8000/EmergencyUser/DroneDispatch/ --accepturl http://$webui_ip:8000/EmergencyControl/control/)
 echo "Mission ID is $mission_id"
-coordinates="$(python $DIR/Tools/MissionMonitor.py --id $mission_id --locationurl http://$webui_ip:8000/EmergencyControl/mission_queue_json/True/ --goal_precision 10 --goal_height 3 --max_mission_time 600 --return_percent 0.2 --print_start --print_goal --print_end | sed ':a;N;$!ba;s/\n/ /g')"
+coordinates=$(python $DIR/Tools/MissionMonitor.py --id $mission_id --locationurl http://$webui_ip:8000/EmergencyControl/mission_queue_json/True/ --goal_precision 10 --goal_height 3 --max_mission_time 600 --return_percent 0.2 --print_start --print_goal --print_end)
+coordinates=$(echo $coordinates | sed ':a;N;$!ba;s/\n/ /g')
 echo "Coordinates: $coordinates"
 start_pos=$(echo "$coordinates" | cut -f1 -d' ')
 goal_pos=$(echo "$coordinates" | cut -f2 -d' ')
