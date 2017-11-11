01a4eb2ec844f4bba9abbe194a1bba205652f265
StefanRvO <stefan@stefanrvo.dk>
2017-11-11 18:55:03 +0100

    Attempt at fixing unpredicatable failures related to parsing the output from mission monitor. Work in TELEM1 loss unit test.

--- tests/gcs_failure_test.sh
+++ tests/gcs_failure_test.sh
@@ -3,11 +3,11 @@ echo "Running GCS failure test"
 $DIR/start_containers.sh slow_bat_drain
 mission_id=$(python $DIR/Tools/start_mission.py --lat 55.562747 --lon 10.113384 --requesturl http://$webui_ip:8000/EmergencyUser/DroneDispatch/ --accepturl http://$webui_ip:8000/EmergencyControl/control/)
 echo "Mission ID is $mission_id"
-coordinates=$(python $DIR/Tools/MissionMonitor.py --id $mission_id --locationurl http://$webui_ip:8000/EmergencyControl/mission_queue_json/True/ --goal_precision 10 --goal_height 3 --max_mission_time 600 --return_percent 0.2 --print_start --print_goal --print_end)
+coordinates="$(python $DIR/Tools/MissionMonitor.py --id $mission_id --locationurl http://$webui_ip:8000/EmergencyControl/mission_queue_json/True/ --goal_precision 10 --goal_height 3 --max_mission_time 600 --return_percent 0.2 --print_start --print_goal --print_end)"
 echo "Coordinates: $coordinates"
-start_pos=$(echo $coordinates | cut -f1 -d$'\n')
-goal_pos=$(echo $coordinates | cut -f2 -d$'\n')
-midway_pos=$(echo $coordinates | cut -f3 -d$'\n')
+start_pos=$(echo "$coordinates" | cut -f1 -d$'\n')
+goal_pos=$(echo "$coordinates" | cut -f2 -d$'\n')
+midway_pos=$(echo "$coordinates" | cut -f3 -d$'\n')
 echo "Drone is 20% of the way. Triggering GCS failure by stoping the container"
 docker stop gcs
 echo "The  drone should now perform a RTL, starting monitoring"
