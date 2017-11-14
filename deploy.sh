a4882dfbd843353d35a547d20a1a0ff3a80bd8e2
StefanRvO <stefan@stefanrvo.dk>
2017-11-14 21:16:52 +0100

    Renamed containers.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 containername=$1
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker tag stefanrvo/$containername:latest stefanrvo/$containername:$TRAVIS_BUILD_NUMBER
-docker tag stefanrvo/$containername:latest stefanrvo/$containername:$TRAVIS_COMMIT
-docker push stefanrvo/$containername:$TRAVIS_BUILD_NUMBER
-docker push stefanrvo/$containername:$TRAVIS_COMMIT
+docker tag stefanrvo/rmuasd_$containername:latest stefanrvo/rmuasd_$containername:$TRAVIS_BUILD_NUMBER
+docker tag stefanrvo/rmuasd_$containername:latest stefanrvo/rmuasd_$containername:$TRAVIS_COMMIT
+docker push stefanrvo/rmuasd_$containername:$TRAVIS_BUILD_NUMBER
+docker push stefanrvo/rmuasd_$containername:$TRAVIS_COMMIT
 
