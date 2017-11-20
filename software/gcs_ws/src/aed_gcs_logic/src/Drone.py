02d239c9f06e06cee5f38992167a227f78408825
Nikolaj <n.bach@sol.dk>
2017-11-20 06:08:48 -0800

    should work again

--- software/gcs_ws/src/aed_gcs_logic/src/Drone.py
+++ software/gcs_ws/src/aed_gcs_logic/src/Drone.py
@@ -91,7 +91,7 @@ class Drone:
                 self.publish_sem.release()
 
     def rpcIsDroneReady(self):
-        if rospy.get_param('/ignore_onboard', False) is True or True :   # remember to remove "or True" both in risk assesment node, ans link monitoring node.
+        if rospy.get_param('/ignore_onboard', False) is True:   # remember to remove "or True" both in risk assesment node, ans link monitoring node.
             return True
         try:
             request = OnboardStatusRequest()
