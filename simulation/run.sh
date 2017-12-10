88b4d6ebf6634587a542a89524bef4c6e279225c
StefanRvO <stefan@stefanrvo.dk>
2017-12-10 16:28:09 +0100

    Added fences for testing at the airport. increased flexability of run files somewhat.

--- simulation/run.sh
+++ simulation/run.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 this_dir=$(pwd)
-export PX4_HOME_LAT=55.5577531
-export PX4_HOME_LON=10.1095593
-export PX4_HOME_ALT=0
+
+if [[ -z "$PX4_HOME_LAT" ]]; then
+	export PX4_HOME_LAT=55.5577531
+fi
+if [[ -z "$PX4_HOME_LON" ]]; then
+	export PX4_HOME_LON=10.1095593
+fi
+if [[ -z "$PX4_HOME_ALT" ]]; then
+	export PX4_HOME_ALT=0
+fi
 source /opt/ros/kinetic/setup.bash
 cd $(pwd)/Firmware/
 make posix_sitl_ekf2_$1 gazebo  -j4 
--- tests/start_containers.sh
+++ tests/start_containers.sh
@@ -1,7 +1,12 @@
 set -e
+
 docker network create --gateway ${networkgateway} --subnet ${networkgateway}/24 ${networkname} || true
 docker run -d --name webui --network ${networkname} --ip ${webui_ip} -e rmq_host=${webui_ip} stefanrvo/rmuasd_webapp:latest
-docker run -d --name simulation --network ${networkname} --ip ${simulation_ip} stefanrvo/rmuasd_gazebo:latest $1
+if [ -z "$PX4_HOME_LAT" ] then
+	docker run -d --name simulation --network ${networkname} --ip ${simulation_ip} stefanrvo/rmuasd_gazebo:latest $1
+else
+	docker run -d --name simulation --network ${networkname} --ip ${simulation_ip} -e PX4_HOME_LAT=${PX4_HOME_LAT} -e PX4_HOME_LON=${PX4_HOME_LON} -e PX4_HOME_ALT=${PX4_HOME_ALT} stefanrvo/rmuasd_gazebo:latest $1
+fi
 sleep 25
 docker run -d --cap-add=NET_ADMIN --name gcs --network ${networkname} -e mavroshost=${simulation_ip}  -e rmq_host=${webui_ip} stefanrvo/rmuasd_groundcontrol:latest
 docker run -d --cap-add=NET_ADMIN --name drone --network ${networkname} -e webui_ip=${webui_ip} -e simulation_ip=${simulation_ip} stefanrvo/rmuasd_drone:latest
